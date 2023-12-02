a8ebdb950643a4aa63b6759e2d5be37a6bdc6cf8
predragstojkovic03 <predragstojkovic.jagodina@gmail.com>
2023-12-02 21:00:30 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT
+
+const app = express();
+app.use(cors());
+
+
+app.listen();
