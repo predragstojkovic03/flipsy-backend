bb432583ef46a00314419c9f280102dc0e50bda0
predragstojkovic03 <predragstojkovic.jagodina@gmail.com>
2023-12-03 04:26:48 +0100

    Moved connectDB call

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 const connectDB = require('./config/db');
-const {notFound, errorHandler} = require("./middleware/errorMiddleware");
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
-const userRoutes = require('./routes/userRoutes')
-const subjectRoutes = require('./routes/subjectRoutes')
+const userRoutes = require('./routes/userRoutes');
+const subjectRoutes = require('./routes/subjectRoutes');
 
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,8 +18,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Api is running...');
-})
+  res.send('Api is running...');
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/subjects', subjectRoutes);
@@ -27,6 +28,5 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
-    connectDB();
+  console.log('Server is listening on port ' + PORT);
 });
